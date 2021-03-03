a2c2c939180851a68dcde018f5e7ac4e544e94f1
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-03-03 12:10:31 +1100

    updated data.py

---
+++ src/data.py
@@ -0,0 +1,38 @@
+users = [
+    '''
+    {
+        'id': 0,
+        'name' : 'user1',
+    }
+    '''
+]
+
+channels = [
+    '''
+    {
+        'name': 'Hayden',
+        'owner_members': [
+            {
+                'u_id': 0,
+                'name_first': 'Hayden',
+                'name_last': 'Jacobs',
+            }
+        ],
+        'all_members': [
+            {
+                'u_id': 0,
+                'name_first': 'Hayden',
+                'name_last': 'Jacobs',
+            }
+        ],
+        'messages': [
+            {
+                'message_id': 0,
+                'u_id': 0,
+                'message_string': 'hello123',
+                'time': 123123123,
+            }
+        ],
+    }
+    '''
+]
