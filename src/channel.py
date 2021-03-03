25e21206a45a56342237f7d9985eea682ee534c2
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-03-04 10:26:46 +1100

    channel_details implementation

--- src/channel.py
+++ src/channel.py
@@ -1,27 +1,79 @@
+from src.data import users, channels
+from src.error import InputError, AccessError
+
+# tests if channel exists
+# returns False if it does
+def test_channel_is_invalid(channel_id):
+    for ch in channels:
+        key, value = 'id', channel_id
+        if key in ch and value == ch[key]:
+            return False
+    return True    
+
+# tests if user exists
+# returns False if they do
+def test_user_is_invalid(u_id):
+    for user in users:
+        key, value = 'id', u_id
+        if key in user and value == user[key]:
+            return False
+    return True  
+
+# tests if user is in a channel
+# returns True if they are
+def test_if_user_in_ch(u_id, channel_id):
+    for user in channels[channel_id]['all_members']:
+        key, value = 'u_id', u_id
+        if key in user and value == user[key]:
+            return True
+    return False    
+
+# give a u_id find a user's details and return them as a dict
+def user_details(u_id):
+    for user in users:
+        key, value = 'u_id', u_id
+        if key in user and value == user[key]:
+            return user
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
+    # invalid channel
+    if test_channel_is_invalid(channel_id):
+        raise(InputError)
+
+    # invalid user
+    if test_user_is_invalid(u_id):
+        raise(InputError)
+
+    # auth user (inviter) is not already a member of the channel
+    if test_if_user_in_ch(auth_user_id, channel_id) == False:
+        raise(AccessError)
+    
+    # adding user to the channel 
+    new_user = user_details(u_id)
+    channels[channel_id]['all_members'].append(new_user)
+
     return {
     }
 
 def channel_details_v1(auth_user_id, channel_id):
-    return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
-    }
+    # invalid channel
+    if test_channel_is_invalid(channel_id):
+        raise(InputError)
 
-def channel_messages_v1(auth_user_id, channel_id, start):
+    # invalid user  
+    if test_if_user_in_ch(auth_user_id, channel_id) == False:
+        raise(AccessError)
+
+    # insert info into dictionary
+    details = {}
+    details['name'] = channels[channel_id]['name']
+    details['owner_members'] = channels[channel_id]['owner_members']
+    details['all_members'] = channels[channel_id]['all_members']
+    
+    return details
+        
+
+def channel_messages_v1(auth_user_id, channel_id, start):      
     return {
         'messages': [
             {
