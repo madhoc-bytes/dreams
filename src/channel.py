e1ea4585980b866b50c28da6bb8d80b7df535361
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-03-05 17:08:33 +1100

    debugged custom functions

--- src/channel.py
+++ src/channel.py
@@ -1,40 +1,6 @@
 from src.data import users, channels
 from src.error import InputError, AccessError
 
-# tests if channel exists 
-# returns False if it does
-def test_channel_is_invalid(channel_id):
-    for ch in channels:
-        key, value = 'id', channel_id
-        if key in ch and value == ch[key]:
-            return False
-    return True    
-
-# tests if user exists
-# returns False if they do
-def test_user_is_invalid(u_id):
-    for user in users:
-        key, value = 'id', u_id
-        if key in user and value == user[key]:
-            return False
-    return True  
-
-# tests if user is in a channel
-# returns True if they are
-def test_if_user_in_ch(u_id, channel_id):
-    for user in channels[channel_id]['all_members']:
-        key, value = 'u_id', u_id
-        if key in user and value == user[key]:
-            return True
-    return False    
-
-# give a u_id find a user's details and return them as a dict
-def user_details(u_id):
-    for user in users:
-        key, value = 'u_id', u_id
-        if key in user and value == user[key]:
-            return user
-
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     # invalid channel
     if test_channel_is_invalid(channel_id):
@@ -58,8 +24,7 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     new_user['name_last'] = new_user_details['name_last']
     channels[channel_id]['all_members'].append(new_user)
 
-    return {
-    }
+    return {}
 
 def channel_details_v1(auth_user_id, channel_id):
     # invalid channel
@@ -108,3 +73,51 @@ def channel_addowner_v1(auth_user_id, channel_id, u_id):
 def channel_removeowner_v1(auth_user_id, channel_id, u_id):
     return {
     }
+
+# CUSTOM FUNCTIONS
+
+# Jeffery's functions
+# ====================================================================
+# tests if channel exists 
+# returns False if it does
+def test_channel_is_invalid(channel_id):
+    for ch in channels:
+        key, value = 'id', channel_id
+        if key in ch and value == ch[key]:
+            return False
+    return True    
+
+# tests if user exists
+# returns False if they do
+def test_user_is_invalid(u_id):
+    if len(users) == 0:
+        return True:
+
+    for user in users:
+        key, value = 'id', u_id
+        if key in user and value == user[key]:
+            return False
+    return True  
+
+# tests if user is in a channel
+# returns True if they are
+def test_if_user_in_ch(u_id, channel_id):
+    if len(channels[channel_id]['all_members']) == 0:
+        return False:
+
+    for user in channels[channel_id]['all_members']:
+        key, value = 'u_id', u_id
+        if key in user and value == user[key]:
+            return True
+    return False    
+
+# give a u_id find a user's details and return them as a dict
+def user_details(u_id):
+    if len(users) == 0:
+        return {}
+
+    for user in users:
+        key, value = 'u_id', u_id
+        if key in user and value == user[key]:
+            return user
+# ====================================================================
