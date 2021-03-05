32180899b89eb638e363d9119e3812629db00309
Baida Du <baida.du@student.unsw.edu.au>
2021-03-05 15:26:30 +1100

    Update other.py

--- src/other.py
+++ src/other.py
@@ -1,5 +1,11 @@
+data = {
+    'users': [],
+    'channels': [],
+}
+''' Resets the internal data of the application to it's initial state '''
 def clear_v1():
-    pass
+    data['users'].clear()
+    data['channels'].clear()
 
 def search_v1(auth_user_id, query_str):
     return {
@@ -12,3 +18,10 @@ def search_v1(auth_user_id, query_str):
             }
         ],
     }
+
+''' create len of channels to check if clear'''
+def data_channels():
+    return len(data['channels'])
+''' create len of users to check if clear'''
+def data_user():
+    return len(data['users'])
