7986ed06476ac020ffb84d38c4e6b32b8a321353
Ojasya Suri <z5230517@ad.unsw.edu.au>
2021-03-15 10:24:25 +1100

    fixed code to get 10/10 pylint

--- src/auth.py
+++ src/auth.py
@@ -1,44 +1,33 @@
-#from src import data 
+'''auth functions implementation'''
 import re
 from src.error import InputError
 from src.data import users
-from src.other import clear_v1
 
 def email_is_valid(email):
+    '''checking for a valid email using regex'''
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    if (re.search(regex, email)):
-        return True
-    else:
-        return False
-
+    bool(re.search(regex, email))
 
 def auth_login_v1(email, password):
-    
+    '''auth login function implementation'''
     if len(users) != 0:
         for user in users:
-            
-            if email_is_valid(email) != True:
+            if not email_is_valid(email):
                 raise InputError
-
-            if (user['email'] == email and user['password'] == password):
+            if user['email'] == email and user['password'] == password:
                 u_id = user['u_id']
-            elif (user['email'] != email):
-                print(email)
-                print(user['email'])
-                #print(u_id)
-                print(user['u_id'])
+            elif user['email'] != email:
                 raise InputError
-            elif (user["password"] != password):
+            elif user["password"] != password:
                 raise InputError
-
-    return (u_id)
+    return u_id
 
 
 def auth_register_v1(email, password, name_first, name_last):
+    '''auth register function implementation'''
     #setting handle
     handle = (name_first + name_last)
     handle = handle.lower()
-    
     if len(users) != 0:
         for user in users:
             if user['email'] == email:
@@ -48,40 +37,25 @@ def auth_register_v1(email, password, name_first, name_last):
     n_users = 0
 
     for user in users:
-        
         unique_suffix = str(n_users)
         handle_len = len(handle)
-
         if user['handle'] == handle:
-            
             if handle_len > 20:
                 handle = (name_first + name_last)[:20] + unique_suffix
-                '''
-                if handle == user['handle']:
-                    handle = handle + unique_suffix
-                '''   
             elif handle_len <= 20:
                 handle = name_first + name_last + unique_suffix
-                #print(n_users)
-                '''
-                if handle == user['handle']:
-                    handle = handle + unique_suffix
-                '''
-        n_users += 1  
-
-    if email_is_valid(email) != True:
-            raise InputError
-    #checking len first and last name less than 50 and more than 1 
+        n_users += 1
+    if not email_is_valid(email):
+        raise InputError
+    #checking len first and last name less than 50 and more than 1
     if len(name_first) > 50 or  len(name_first) <= 1 or len(name_last) > 50 or len(name_last) <= 1:
         raise InputError
-    
     if not name_first.strip() or not name_last.strip():
         raise InputError
 
     #checking valid pass len
     if len(password) < 6:
-            raise InputError
-
+        raise InputError
 
     auth_user_id = len(users)
 
