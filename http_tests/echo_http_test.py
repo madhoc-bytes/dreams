11a6d7739981972d597727e9a6be12926532bb8f
Dylan Chan <z5309393@unsw.edu.au>
2021-04-04 16:51:19 +1000

    updated echo http test

--- http_tests/echo_http_test.py
+++ http_tests/echo_http_test.py
@@ -6,8 +6,6 @@ from src import config
 def test_echo():
     '''
     A simple test to check echo
-    
+    '''
     resp = requests.get(config.url + 'echo', params={'data': 'hello'})
     assert json.loads(resp.text) == {'data': 'hello'}
-    '''
-    pass
