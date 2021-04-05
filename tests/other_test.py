afbf2db0d769508e54095dc7d33e4c83fcc1d7a4
Baida Du <baida.du@student.unsw.edu.au>
2021-04-05 12:49:26 +1000

    Update other_test.py

--- tests/other_test.py
+++ tests/other_test.py
@@ -1,7 +1,7 @@
-from src.other import clear_v1, data_channels, data_user
+from src.other import clear_v2, data_channels, data_user
 import pytest
 
-def test_clear_v1():
-    clear_v1()
+def test_clear_v2():
+    clear_v2()
     assert data_channels() == 0
     assert data_user() == 0
