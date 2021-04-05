12366d8af0debc0d6e0f2863d83e083bedccc426
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 03:24:54 +1000

    Update channels_create_http_test.py

--- http_tests/channels_create_http_test.py
+++ http_tests/channels_create_http_test.py
@@ -4,7 +4,7 @@ import json
 from src import config
 import flask 
 
-def test_join_channel(): 
+def test_channels_create_public(): 
     requests.delete(config.url + 'clear/v1')
 
     # register a user
@@ -30,7 +30,7 @@ def test_join_channel():
 
     assert r.status_code == 200
 
-def test_join_private():
+def test_channels_create_private():
     requests.delete(config.url + 'clear/v1')
 
     # register a user
@@ -50,6 +50,32 @@ def test_join_private():
         'token': token,
         'name': 'test_ch',
         'is_public': False
+    }
+
+    r = requests.post(config.url + 'channels/create/v2', json=ch_data)
+
+    assert r.status_code == 200
+    
+def test_channels_create_invalid():
+    requests.delete(config.url + 'clear/v1')
+
+    # register a user
+    reg_data = {
+        'email': 'test@gmail.com',
+        'password': 'testpw123',
+        'name_first': 'test_fname',
+        'name_last': 'test_lname'
+    }
+
+    # acquire token and id of user
+    r = requests.post(config.url + 'auth/register/v2', json=reg_data)
+    token = r.json().get('token')
+    
+    # create a private channel
+    ch_data = {
+        'token': token,
+        'name': 'qweasdzxcvbnfghrtyuiojklmp',
+        'is_public': True
     }
 
     r = requests.post(config.url + 'channels/create/v2', json=ch_data)
