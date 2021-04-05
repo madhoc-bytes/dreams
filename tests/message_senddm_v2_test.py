0719011a6361d6dc24d1d4613f2321d168e03059
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 04:32:03 +1000

    Update message_senddm_v2_test.py

--- tests/message_senddm_v2_test.py
+++ tests/message_senddm_v2_test.py
@@ -8,6 +8,7 @@ from src.message_senddm_v2 import message_senddm_v2
 def test_long_1000_dm_messsages():
     clear_v2()
     information1 = auth_register_v2("dubaida28951345@gmail.com", "xujiawen", "Jemma", "Simmons")
+    auth_token = information1['token']
     user_id = auth_register_v2('test_user@gmail.com', 'test_pw_user', 'userf', 'userl')['auth_user_id']
     u_ids = [user_id]
     # create a test dm
