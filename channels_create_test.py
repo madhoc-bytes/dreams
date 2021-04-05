a88b5df0947b2aeeb0f100dbe1802350183e38bc
Baida Du <baida.du@student.unsw.edu.au>
2021-04-05 15:32:09 +1000

    Update channels_test_create_needothersfunction.py

---
+++ channels_create_test.py
@@ -0,0 +1,21 @@
+import pytest
+from src.channels import channels_create_v2 
+from src.auth import auth_register_v2
+from src.error import InputError, AccessError
+from src.other import clear_v2
+
+# test if channels can be created correctly
+def test_channels_create_v2():
+    clear_v2()
+    auth_user_id00 = auth_register_v2("dubaida28951345@gmail.com", "xujiawen", "Jemma", "Simmons")
+    assert channels_create_v2(auth_user_id00['token'], 'first', True) == {'id' : 0}
+    assert channels_create_v2(auth_user_id00['token'], 'second', False) == {'id' : 1}
+
+# test the error stituation
+def tests_channels_create_v2_except():
+    clear_v2()
+    auth_user_id11 = auth_register_v2("dubaida28951345@gmail.com", "xujiawen", "Jemma", "Simmons")
+    with pytest.raises(InputError):
+        channels_create_v2(auth_user_id11['token'], "kasbfvkabvadfihviadfvbhidfbuiva", False)
+    with pytest.raises(AccessError):
+        channels_create_v2(auth_user_id11['token'] + 'a', 'first', True)
