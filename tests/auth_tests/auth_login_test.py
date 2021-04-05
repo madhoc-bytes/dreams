5a7f1226321a1d6a0c53ce76af7d6bb70f0bc44b
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-04-05 23:09:07 +1000

    removed old tests

--- tests/auth_tests/auth_login_test.py
+++ tests/auth_tests/auth_login_test.py
@@ -1,63 +1,15 @@
-import pytest 
-from src.auth import auth_login_v1, auth_register_v1
-from src.error import InputError
-from src.data import users
-from src.other import clear_v1
-
 
 def test_auth_login_valid():
-    clear_v1()
-    valid_email, valid_password = 'abc@def.com', 'helloWorld123!'
-    valid_first_name, valid_last_name = 'john', 'smith'
-    new_user_id = auth_register_v1(
-            valid_email, 
-            valid_password, 
-            valid_first_name, 
-            valid_last_name)['auth_user_id']
-
-    assert auth_login_v1(valid_email, valid_password) == new_user_id
+    pass
 
 def test_valid_email():
-    clear_v1()
-    valid_email, valid_password = 'abc@def.com', 'helloWorld123!'
-    valid_first_name, valid_last_name = 'john', 'smith'
-    invalid_email = 'abc.com'
-    auth_register_v1(
-            valid_email, 
-            valid_password, 
-            valid_first_name, 
-            valid_last_name)['auth_user_id']
-
-    with pytest.raises(InputError):
-        auth_login_v1(invalid_email, valid_password)
+    pass
 
 def test_wrong_email():
-    clear_v1()
-    valid_email, valid_password = 'abc@def.com', 'helloWorld123!'
-    valid_first_name, valid_last_name = 'john', 'smith'
-    wrong_email = 'def@def.com'
-    auth_register_v1(
-            valid_email, 
-            valid_password, 
-            valid_first_name, 
-            valid_last_name)['auth_user_id']
-
-    with pytest.raises(InputError):
-        auth_login_v1(wrong_email, valid_password)
+    pass
     
 def test_wrong_password():
-    clear_v1()
-    valid_email, valid_password = 'abc@def.com', 'helloWorld123!'
-    valid_first_name, valid_last_name = 'john', 'smith'
-    wrong_password = 'helloWorld1'
-    auth_register_v1(
-            valid_email, 
-            valid_password, 
-            valid_first_name, 
-            valid_last_name)['auth_user_id']
-
-    with pytest.raises(InputError):
-        auth_login_v1(valid_email, wrong_password)
+    pass
 
 
         
