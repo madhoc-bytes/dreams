557253f3290ad34026b97f153721d46c6fed8379
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 03:19:56 +1000

    Update channels_create_http_test.py

--- http_tests/channels_create_http_test.py
+++ http_tests/channels_create_http_test.py
@@ -18,7 +18,6 @@ def test_join_channel():
     # acquire token and id of user
     r = requests.post(config.url + 'auth/register/v2', json=reg_data)
     token = r.json().get('token')
-    u_id = r.json().get('auth_user_id')
     
     # create a channel
     ch_data = {
@@ -30,3 +29,29 @@ def test_join_channel():
     r = requests.post(config.url + 'channels/create/v2', json=ch_data)
 
     assert r.status_code == 200
+
+def test_join_private():
+    requests.delete(config.url + 'clear/v1')
+
+    # register a user
+    reg_data = {
+        'email': 'test@gmail.com',
+        'password': 'testpw123',
+        'name_first': 'test_fname',
+        'name_last': 'test_lname'
+    }
+
+    # acquire token and id of user
+    r = requests.post(config.url + 'auth/register/v2', json=reg_data)
+    token = r.json().get('token')
+    
+    # create a private channel
+    ch_data = {
+        'token': token,
+        'name': 'test_ch',
+        'is_public': False
+    }
+
+    r = requests.post(config.url + 'channels/create/v2', json=ch_data)
+
+    assert r.status_code == 400
