e6e6b269282f42f223b8ef5790a9d3529e2cc2cc
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 03:16:39 +1000

    Update channels_create_http_test.py

---
+++ http_tests/channels_create_http_test.py
@@ -0,0 +1,32 @@
+import pytest
+import requests
+import json
+from src import config
+import flask 
+
+def test_join_channel(): 
+    requests.delete(config.url + 'clear/v1')
+
+    # register a user
+    reg_data = {
+        'email': 'test@gmail.com',
+        'password': 'testpw123',
+        'name_first': 'test_fname',
+        'name_last': 'test_lname'
+    }
+
+    # acquire token and id of user
+    r = requests.post(config.url + 'auth/register/v2', json=reg_data)
+    token = r.json().get('token')
+    u_id = r.json().get('auth_user_id')
+    
+    # create a channel
+    ch_data = {
+        'token': token,
+        'name': 'test_ch',
+        'is_public': True
+    }
+
+    r = requests.post(config.url + 'channels/create/v2', json=ch_data)
+
+    assert r.status_code == 200
