601255091182fade834667d4efb68b045f764001
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 03:55:54 +1000

    Update message_senddm_v2_test.py

--- message_senddm_v2_test.py
+++ message_senddm_v2_test.py
@@ -11,11 +11,3 @@ def test_long_1000_dm_messsages():
     dm_id1 = dm_create_v2(information1['token'], 'long_1000_dm_messsages', True)
     with pytest.raises(InputError):
         assert message_senddm_v2(information1['token'], dm_id1['dm_id'],'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-
-def not_owner_send_dm_message():
-    clear_v2()
-    information1 = auth_register_v2("dubaida28951345@gmail.com", "xujiawen", "Jemma", "Simmons")
-    dm_id1 = dm_create_v2(information1['token'], 'long_1000_dm_messsages', True)
-    information2 = auth_register_v2("sdfgggh@gmail.com", "sdfgh", "wer", "ghshh")
-    with pytest.raises(AccessError):
-        assert message_senddm_v2(information2['token'], dm_id1['dm_id'], 'See')
