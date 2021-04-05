dbf5d5a26d781c82307ab26d5befac87440c0975
Baida Du <baida.du@student.unsw.edu.au>
2021-04-05 16:40:36 +1000

    Update dm_create_v2.py

--- src/dm_create_v2.py
+++ src/dm_create_v2.py
@@ -1,16 +1,14 @@
-from src.channel import token_to_id
+from src.channel import token_to_id, test_user_is_invalid
 from src.error import InputError, AccessError
 from src.data import dms, users
 
 def dm_create_v2(token, u_ids):
-    token_uid = token_to_id(token)
-    user_creator = check_if_user_exit(token_uid)
+    user = token_to_id(token)
     handlelist = []
     userlist = []
 
     for userid in u_ids:
-        user = check_if_user_exit(userid)
-        if not user:
+        if not test_user_is_invalid(userid):
             raise InputError('userid does not refer to a valid user')
         handlelist.append(user['handle'])
         userlist.append(user)
@@ -20,7 +18,7 @@ def dm_create_v2(token, u_ids):
 
     new_dm = {
         'dm_id': dm_id,
-        'creator':user_creator
+        'creator':user
         'dm_name': new_dm_handle,
         'all_dm_members': userlist,
         'messages':[],
