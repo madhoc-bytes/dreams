a3fd4b6eb45cabbdc26ce1e66cff77f30be19c53
Dylan Chan <z5309393@unsw.edu.au>
2021-04-06 01:14:44 +1000

    http tests fo dm list

---
+++ http_tests/dm_list_http_test.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+import json
+from src import config
+import flask 
+
+def test_dm_list_basic():    
+    requests.delete(config.url + 'clear/v1')
+
+    #owner of dm/caller of dm_create
+    reg_data = {
+        'email': 'test_auth@gmail.com',
+        'password': 'test_pw_auth',
+        'name_first': 'testf',
+        'name_last': 'testl'
+    }
+
+    r = requests.post(config.url + 'auth/register/v2', json=reg_data)
+    token = r.json().get('token')
+    u_id = r.json().get('auth_user_id')
+
+    list_params = {
+        'token': token
+    }
+
+    r = requests.get(config.url + 'channel/list/v2', json=list_params)
