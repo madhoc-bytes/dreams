371a236168e6f8383701b441aea029570778ea99
Baida Du <baida.du@student.unsw.edu.au>
2021-04-05 17:03:01 +1000

    Update test_userpermission_change.py

--- tests/test_userpermission_change.py
+++ tests/test_userpermission_change.py
@@ -1,36 +1,42 @@
-# test if the admin_userpermission_change correctly
-def test_admin_userpermission_change():
+import pytest
+from src.auth import auth_register_v2
+from src.other import clear_v2
+from src.error import InputError, AccessError
+from src.admin_userpermission_change_v1 import adminuserpermissionchangev1, change_permission
+
+# test if the adminuserpermissionchangev1 correctly
+def test_adminuserpermissionchangev1():
     clear_v2()
     information_user = auth_register_v2("dubaida@gmail.com", "xujiawen", "qwer", "Du")
     auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
-    assert data_permission(1) == 2
-    admin_userpermission_change(information_user['token'], 1, 1)
-    assert data_permission(1) == 1
+    assert change_permission(1) == 2
+    adminuserpermissionchangev1(information_user['token'], 1, 1)
+    assert change_permission(1) == 1
 
-def test_admin_userpermission_change_invalid_token():
+def test_adminuserpermissionchangev1_invalid_token():
     clear_v2()
     information_user = auth_register_v2("dubaida@gmail.com", "xujiawen", "qwer", "Du")
     auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
     with pytest.raises(AccessError):
-        admin_userpermission_change(information_user['token'] + 'a', 1, 1)
+        adminuserpermissionchangev1(information_user['token'] + 'a', 1, 1)
 
-def test_admin_userpermission_change_invalid_user():
+def test_adminuserpermissionchangev1_invalid_user():
     clear_v2()
     information_user = auth_register_v2("dubaida@gmail.com", "xujiawen", "qwer", "Du")
     auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
     with pytest.raises(InputError):
-        admin_userpermission_change(information_user['token'], 3, 1)
+        adminuserpermissionchangev1(information_user['token'], 3, 1)
 
-def test_admin_userpermission_change_invalid_permission():
+def test_adminuserpermissionchangev1_invalid_permission():
     clear_v2()
     information_user = auth_register_v2("dubaida@gmail.com", "xujiawen", "qwer", "Du")
     auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
     with pytest.raises(InputError):
-        admin_userpermission_change(information_user['token'], 1, 3)
+        adminuserpermissionchangev1(information_user['token'], 1, 3)
 
-def test_admin_userpermission_change_not_oDuner():
+def test_adminuserpermissionchangev1_not_owner():
     clear_v2()
     auth_register_v2("dubaida@gmail.com", "xujiawen", "qwer", "Du")
-    info2 = auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
+    information_user = auth_register_v2("kendelle@qwef.com", "sdfgwg334", "BDSF", "Kendelle")
     with pytest.raises(AccessError):
-        admin_userpermission_change(info2['token'], 0, 2)
+        adminuserpermissionchangev1(information_user['token'], 0, 2)
