a842fa4ad9b4c0efb606ce4cf45e2d3e2e2eb8f2
Baida Du <baida.du@student.unsw.edu.au>
2021-04-05 12:54:25 +1000

    Update channels_test_create_needothersfunction.py

--- tests/channels_test_create_needothersfunction.py
+++ tests/channels_test_create_needothersfunction.py
@@ -7,7 +7,7 @@ from src.other import clear_v2
 
 # test if channels can be created correctly
 def test_channels_create_v2():
-    clear_21()
+    clear_2()
     c1 = auth_register_v1("dubaida28951345@gmail.com", "xujiawen", "Jemma", "Simmons")
     assert channels_create_v2(c1['token'], 'first', True) == {'id' : 0}
     assert channels_create_v2(c1['token'], 'second', False) == {'id' : 1}
