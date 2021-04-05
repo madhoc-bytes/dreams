1944fa6f0b4789007c480144233c7bd26479636e
Baida Du <baida.du@student.unsw.edu.au>
2021-04-06 05:45:17 +1000

    Update auth_logout_test.py

--- tests/auth_logout_test.py
+++ tests/auth_logout_test.py
@@ -2,11 +2,11 @@ import pytest
 from src.auth import auth_login_v2, auth_register_v2, auth_logout_v2
 from src.error import InputError
 from src.data import users
-from src.other import clear_v1
+from src.other import clear_v2
 
 
 def test_auth_logout_valid():
-    clear_v1()
+    clear_v2()
     valid_email, valid_password = 'abc@def.com', 'helloWorld123!'
     valid_first_name, valid_last_name = 'john', 'smith'
     auth_register_v2(valid_email, valid_password, valid_first_name, valid_last_name)
