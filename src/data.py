9a8d07d2bf234bae167511f5c6fb5a639d253456
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-04-14 18:25:58 +1000

    tested and implemented persistence of data

--- src/data.py
+++ src/data.py
@@ -1,67 +1,83 @@
-users = []
-'''
-{
-    'email': 'emai@email.com',
-    'password': 'Password1',
-    'name_first': 'user',
-    'name_last': 'name',
-    'handle': 'username',
-    'u_id': 0,
-}
-'''
-    
+import os 
+import pickle
 
+# not persistent
+if not os.path.isfile('persistent_data.p'):
+    users = []
+    # example of what users list with 1 user would look like
+    '''
+    {
+        'email': 'emai@email.com',
+        'password': 'Password1',
+        'name_first': 'user',
+        'name_last': 'name',
+        'handle': 'username',
+        'u_id': 0,
+    }
+    '''
+    channels = []
+    '''
+    {   
+        'id': 0,
+        'is_public': True,
+        'name': 'Hayden',
+        'owner_members': [
+            {
+                'u_id': 0,
+                'name_first': 'Hayden',
+                'name_last': 'Jacobs',
+            }
+        ],
+        'all_members': [
+            {
+                'u_id': 0,
+                'name_first': 'Hayden',
+                'name_last': 'Jacobs',
+            }
+        ],
+        'messages': [
+            {
+                'message_id': 0,
+                'u_id': 0,
+                'message_string': 'hello123',
+                'time': 123123123,
+            }
+        ],
+    }
+    '''
+    dms = []
+    '''
+    {   
+        'dm_id': 0,
+        'dm_name': 'Hayden',
+        'owner_id': id
+        'all_members': [
+            {
+                'u_id': 0,
+                'name_first': 'Hayden',
+                'name_last': 'Jacobs',
+            }
+        ],
+        'messages': [
+            {
+                'message_id': 0,
+                'u_id': 0,
+                'message_string': 'hello123',
+                'time': 123123123,
+            }
+        ],
+    }
+    '''
+# persistent
+else:
+    with open('persistent_data.p', 'rb') as file:
+        users = pickle.load(file)
+        channels = pickle.load(file)
+        dms = pickle.load(file)
 
-channels = []
-'''
-{   
-    'id': 0,
-    'is_public': True,
-    'name': 'Hayden',
-    'owner_members': [
-        {
-            'u_id': 0,
-            'name_first': 'Hayden',
-            'name_last': 'Jacobs',
-        }
-    ],
-    'all_members': [
-        {
-            'u_id': 0,
-            'name_first': 'Hayden',
-            'name_last': 'Jacobs',
-        }
-    ],
-    'messages': [
-        {
-            'message_id': 0,
-            'u_id': 0,
-            'message_string': 'hello123',
-            'time': 123123123,
-        }
-    ],
-}
-'''
-dms = []
-'''
-{   
-    'dm_id': 0,
-    'dm_name': 'Hayden',
-    'owner_id': id
-    'all_members': [
-        {
-            'u_id': 0,
-            'name_first': 'Hayden',
-            'name_last': 'Jacobs',
-        }
-    ],
-    'messages': [
-        {
-            'message_id': 0,
-            'u_id': 0,
-            'message_string': 'hello123',
-            'time': 123123123,
-        }
-    ],
-}
-'''
+def persist_data():
+    with open('persistent_data.p', 'wb') as file:
+        pickle.dump(users, file)
+        pickle.dump(channels, file)
+        pickle.dump(dms, file)
+
