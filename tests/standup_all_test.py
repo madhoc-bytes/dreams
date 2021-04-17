b214f4c40aa0bf224774062185c0aeea92bfdd3a
Baida Du <baida.du@student.unsw.edu.au>
2021-04-17 16:16:33 +1000

    Update standup_all_test.py

--- tests/standup_all_test.py
+++ tests/standup_all_test.py
@@ -64,31 +64,6 @@ def test_standup_active_invalidchannel_id():
     with pytest.raises(InputError):
         standup_active_v1(user1_token, channel_item)
 
-def test_standup_active():
-    #clear
-    clear_v2()
-
-    #create and login in user
-    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qwerasdf', 'Dyu', 'Baidas')
-    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qwerasdf')
-    user1_token = user1['token']
-
-    #create two channels 
-    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
-    channel_user2_id = channels_create_v2(user1_token,'gitb2',True).get('channel_id')
-
-    #start a standup 
-    standup_1 = standup_start_v1(user1_token, channel_user1_id, 2)     
-
-    standupactive1 = standup_active_v1(user1_token, channel_user1_id)
-    assert standupactive1['is_active'] == True
-    assert standupactive1['finishtime'] == standup_1['finishtime']
-
-    standupactive2 = standup_active_v1(user1_token, channel_user2_id)
-    assert standupactive2['is_active'] == False
-    assert standupactive2['finishtime'] == None
-
-
 def test_stand_send_invalidchannelid():
     #clear
     clear_v2()
