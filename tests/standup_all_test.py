2add486fd934eba2620971ae59780d44cdd286e6
Baida Du <baida.du@student.unsw.edu.au>
2021-04-17 15:02:11 +1000

    Upload New File

---
+++ tests/standup_all_test.py
@@ -0,0 +1,172 @@
+import pytest
+from src.channels import channels_create_v2 
+from src.other import clear_v2
+from src.auth import auth_register_v2, auth_login_v2
+from src.error import InputError, AccessError
+from src.standup import standup_active_v1, standup_start_v1, standup_send_v1
+
+def test_standup_start_invalidtoken():
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'Dyu', 'Baidas')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create the channel.
+    channel_user1_id = channels_create_v2(user1_token, 'gitb' , True).get('channel_id')
+
+    token_item = user1_token + ' '
+    with pytest.raises(InputError):
+        standup_start_v1(token_item, channel_user1_id, 1)
+
+def test_standup_start_invalidchannelid():
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'Dyu', 'Baidas')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create the channel.
+    channel_user1_id = channels_create_v2(user1_token, 'gitb' , True).get('channel_id')
+
+    channel_item = channel_user1_id + 1
+    with pytest.raises(InputError):
+        standup_start_v1(user1_token, channel_item, 1)
+
+def test_standup_start():
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'Dyu', 'Baidas')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create the channel.
+    channel_user1_id = channels_create_v2(user1_token, 'gitb' , True).get('channel_id')
+
+    standup_start_v1(user1_token, channel_user1_id, 3)
+
+    with pytest.raises(InputError):
+        standup_start_v1(user1_token, channel_user1_id, 1)
+
+def test_standup_active_invalidchannel_id():
+    #clear_v2
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'Dyu', 'Baidas')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create the channel.
+    channel_user1_id = channels_create_v2(user1_token, 'gitb' , True).get('channel_id')
+
+    #start a standup
+    standup_start_v1(user1_token, channel_user1_id, 1)
+
+    channel_item = channel_user1_id + 1
+    with pytest.raises(InputError):
+        standup_active_v1(user1_token, channel_item)
+
+def test_standup_active():
+    #clear
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'Dyu', 'Baidas')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create two channels 
+    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
+    channel_user2_id = channels_create_v2(user1_token,'gitb2',True).get('channel_id')
+
+    #start a standup 
+    standup_1 = standup_start_v1(user1_token, channel_user1_id, 2)     
+
+    standupactive1 = standup_active_v1(user1_token, channel_user1_id)
+    assert standupactive1['is_active'] == True
+    assert standupactive1['finishtime'] == standup_1['finishtime']
+
+    standupactive2 = standup_active_v1(user1_token, channel_user2_id)
+    assert standupactive2['is_active'] == False
+    assert standupactive2['finishtime'] == None
+
+
+def test_send_invalid_channel_id():
+    #clear
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'du', 'asdf')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create a channel 
+    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
+
+    #start a standup
+    standup_start_v1(user1_token, channel_user1_id, 1)
+
+    channel_item = channel_user1_id + 1
+    with pytest.raises(InputError):
+        standup_send_v1(user1_token, channel_item,'')
+
+def test_send_no_active():
+    #clear
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'df', 'asdf')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create a channel 
+    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
+
+    with pytest.raises(InputError):
+        standup_send_v1(user1_token, channel_user1_id,'')
+
+def test_send_not_member():
+    '''test for accessing the error when the user is not the member in the channel'''
+    clear_v2()
+
+    #create and login in two users
+    user1 = auth_register_v2('dbdqwe3@gmail.com', 'qweasd', 'df', 'asdf')
+    user1 = auth_login_v2('dbdqwe3@gmail.com', 'qweasd')
+    user1_token = user1['token']
+    user2 = auth_register_v2('qweaqwed@gmail.com', 'asd', 'df2', 'asdf2')
+    user2 = auth_login_v2('qweaqwed@gmail.com', 'asd')
+    user2_token = user2['token']
+
+    #create a channel 
+    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
+
+    #start a standup
+    standup_start_v1(user1_token, channel_user1_id, 1)
+
+    with pytest.raises(AccessError):
+        standup_send_v1(user2_token, channel_user1_id,'')
+
+def test_standup_long_message():
+    '''test for inputing the error that the message is more than 1000 words'''
+    clear_v2()
+
+    #create and login in user
+    user1 = auth_register_v2('qweasd@gmail.com', 'qweasd', 'df', 'asdf')
+    user1 = auth_login_v2('qweasd@gmail.com', 'qweasd')
+    user1_token = user1['token']
+
+    #create a channel 
+    channel_user1_id = channels_create_v2(user1_token,'gitb',True).get('channel_id')
+
+    #start a standup
+    standup_start_v1(user1_token, channel_user1_id, 1)
+
+    #longmessage check
+    message = "qwera"
+    message = 2000 * message
+    with pytest.raises(InputError):
+        standup_send_v1(user1_token, channel_user1_id, message)
